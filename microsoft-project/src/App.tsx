995d00dc710a318df0cf8ec86077cc5d8802dae1
Julian Rojas <rojasmolinajulian@gmail.com>
2018-04-26 02:22:43 -0700

    finalized algorithm

--- microsoft-project/src/App.tsx
+++ microsoft-project/src/App.tsx
@@ -5,9 +5,14 @@ import './App.css';
 // }
 interface IState {
   newString: string;
+  outcome: string;
+  isFailHidden: boolean;
+  isSuccessHidden: boolean;
 }
 
 class App extends React.Component <{}, IState>{
+
+  public myString: string;
 
   constructor(props:any) {
     super(props);
@@ -16,20 +21,95 @@ class App extends React.Component <{}, IState>{
     this.handleChange = this.handleChange.bind(this);
     
     this.state = {
-      newString: ""
+      isFailHidden: true,
+      isSuccessHidden: true,
+      newString: "",
+      outcome: "",
     };
 
   }
+  public createHistogram(inputString: string) : number[]{
+    let i : number;
+    const histogram = new Array<number>(128).fill(0);
+
+    for(i = 0; i <= inputString.length - 1; i++){
+      histogram[inputString[i].charCodeAt(0)]++;
+    }
+    console.log(histogram);
+    return histogram;
+  }
+
+  public createNewRepeatedCharString(inputString: string, histogram: number[]) : string{
+    let i : number;
+    let repeatedCharString: string = "";
+
+    for(i = 0; i <= inputString.length - 1; i++){
+      if(histogram[inputString[i].charCodeAt(0)] > 1){
+        repeatedCharString += inputString[i];      
+      }
+    } 
+    console.log(repeatedCharString);
+    return repeatedCharString;
+  }
+
+  public isRepeatedCharArrayAreflection(repeatedChars: string) : boolean {
+    let halfway: number;
+    let i: number;
+
+    if(repeatedChars){
+      halfway = Math.ceil(repeatedChars.length / 2.0);
+      
+      for(i=0; i<= halfway; i++){
+        if(repeatedChars[i] !== repeatedChars[repeatedChars.length - i]){
+          return false;
+        }
+      }
+    }
+    console.log(repeatedChars);
+    return true;
+  }
+
+  public isSubsequenceRepeated(repeatedChars: string) : boolean {
+    if (this.isRepeatedCharArrayAreflection(repeatedChars)){
+      return false
+    }
+    return true;
+  }
+
 
   public handleFindout(event:any){
     event.preventDefault();
     
     if (this.state.newString){
-      // DO SOmething
-      alert("String is not empty: " + this.state.newString);
+      let isSubsequenceRepeated: boolean = false;
+
+      this.myString = this.state.newString.toLowerCase();
+      console.log(this.myString);
+      const histogram: number[] = this.createHistogram(this.myString);
+      const repeatedCharsString: string = this.createNewRepeatedCharString(this.myString, histogram);
+
+      if(repeatedCharsString && repeatedCharsString.length >= 4){
+        isSubsequenceRepeated = this.isSubsequenceRepeated(repeatedCharsString)
+      }
+
+      if(isSubsequenceRepeated){
+        this.setState({
+          isSuccessHidden: !isSubsequenceRepeated,
+          isFailHidden: isSubsequenceRepeated,
+          outcome: "YES A SUBSET IS REPEATED",
+        })
+      }
+
+      else{
+        this.setState({
+          isSuccessHidden: !isSubsequenceRepeated,
+          isFailHidden: isSubsequenceRepeated,
+          outcome: "NO",
+        })
+      }
     }
     else{
-      alert("string is empty");
+      alert("string is empty!");
     }
   }
 
@@ -45,7 +125,7 @@ class App extends React.Component <{}, IState>{
         <header className="App-header">
           <h1>Find Out If String Includes Subset</h1>
         </header>
-        <p className="App-intro">
+        <div className="App-intro">
           <div className="col-md-6 offset-md-3"> 
             <form className="pt-4">
               <div className="form-group row">
@@ -62,7 +142,21 @@ class App extends React.Component <{}, IState>{
               </div>
             </form>
           </div>
-        </p>
+          <div className="col-md-6 offset-md-3">
+            <div className="col-md-6" hidden={this.state.isFailHidden}>
+              <div className="alert alert-dismissible alert-danger">
+                <button type="button" className="close" data-dismiss="alert">&times;</button>
+                <strong>Oh snap!</strong> <a href="#" className="alert-link">{this.state.outcome}</a>
+              </div>
+            </div>
+            <div className="col-md-6" hidden={this.state.isSuccessHidden}> 
+              <div className="alert alert-dismissible alert-success">
+                <button type="button" className="close" data-dismiss="alert">&times;</button>
+                <strong>YEAHH</strong> <a href="#" className="alert-link">{this.state.outcome}</a>
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
     );
   }
